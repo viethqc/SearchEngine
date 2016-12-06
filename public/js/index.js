82c556b4213deaceab85e1e81c2d5743c69cd316
viethqc <hoangvietbk2.06@gmail.com>
2016-12-07 02:26:13 +0700

    Them ham HighLight text

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ $( document ).ready(function()
 	var MAX_SHOW_PAGE = 5;
 	var MAX_RESULT_SHOW = 10;
 	var m_iCurrent = 1;							//Current page;
+	var m_bAdvanceSearch = false;
 
 	$(".page-manager").hide();
 	$(".line").hide();
@@ -18,7 +19,12 @@ $( document ).ready(function()
 	});
 
 	$(".advance-search").click(function(){
-		$(".collapse").show();
+		m_bAdvanceSearch = !m_bAdvanceSearch;
+
+		if (m_bAdvanceSearch == true)
+			$(".collapse").show();
+		else
+			$(".collapse").hide();
 	});
 
 	$('#text-search').keypress(function (e) {
@@ -29,6 +35,12 @@ $( document ).ready(function()
 	  		m_iCurrent = 1;
 		    Search(szTextSearch, 1);
 	  	}
+	});
+
+	$(".search-button-icon").click(function(){
+		var szTextSearch = $("#text-search").val();
+		m_iCurrent = 1;
+		Search(szTextSearch, 1);
 	});
 
 	$("#search-btn").click(function(){
@@ -103,6 +115,19 @@ $( document ).ready(function()
 	    
 	    reg = new RegExp(szKeywordSearch, "img");
 
+	    if (objDoc.length == 0)
+	    {
+	    	$(".result").html(" Không tìm thấy <b>" + szSearchText +"</b> trong tài liệu nào<br>" 		+ 
+	    						"Ðề xuất:"																+															
+								"<ul>"																	+
+									"<li>Xin bạn chắc chắn rằng tất cả các từ đều đúng chính tả.</li>"	+	
+									"<li>Hãy thử những từ khóa khác.</li>"								+								
+									"<li>Hãy thử những từ khóa chung hơn.</li>"							+
+									"<li>Hãy thử bớt từ khóa.</li>"										+									
+								"</ul>");
+	    	return;
+	    }
+
 	    $(".result").text("");
 	    for (var i = 0; i < objDoc.length; i++)
 	    {
@@ -110,7 +135,7 @@ $( document ).ready(function()
 	    	szHtml += '<div class="single-result">';
 	    	szHtml += '<div class="title"><a href = "' + objDoc[i]["url"] + '" target = "_blank">' + objDoc[i]["title"] + '</a></div>';
 	    	szHtml += '<div class="url">' + objDoc[i]["url"] + '</div>';
-	    	szHtml += '<div class="content">' + objDoc[i]["content"][0] + "</div>";
+	    	szHtml += '<div class="content">' + HighLight(objDoc[i]["content"][0], szSearchText) + "</div>";
 	    	szHtml += '</div>';
 	    }
 
@@ -181,6 +206,123 @@ $( document ).ready(function()
 	    l.href = href;
 	    return l;
 	};
+
+	function HighLight(strDocument, strKeyword)
+	{
+		var arrParts = [];
+		var arrPartDoc = [];
+		var strDocAfferHighLight;
+		var arrPartDocAfferHighLight = [];
+		var strResult;
+
+		//Tim tung phan cua tu tim kiem
+		arrParts = strKeyword.split(" ");
+		arrPartDoc  = strDocument.split(".");
+		var arrRank = [];
+		var iCount = 0;
+		var objRank = {};
+		var arrObj = [];
+
+		//Tim tan suat xuat hien cua tu trong tung cau
+		for (var i = 0; i < arrPartDoc.length; i++)
+		{
+			iCount = 0;
+			for (var j = 0; j < arrParts.length; j++)
+			{
+				if (arrPartDoc[i].indexOf(arrParts[j]) == -1)
+				{
+					continue;
+				}
+				iCount++;
+			}
+			arrRank[i] = iCount;
+		}
+
+		//Day thong tin xep hang vao mang object
+		for (var i = 0; i < arrRank.length; i++)
+		{
+			arrObj.push({id : i, rank : arrRank[i]});
+		}
+
+		//Sap xep mang object theo rank
+		arrObj.sort(function(a, b){
+			return b.rank - a.rank;
+		});
+
+		strDocAfferHighLight = HighLightSentence(strDocument, strKeyword);
+		arrPartDocAfferHighLight = strDocAfferHighLight.split(".");
+
+		strResult = "";
+		for (var i = 0; i < arrObj.length; i++)
+		{
+			if (arrObj[i].rank == 0)
+			{
+				break;
+			}
+
+			strResult += arrPartDocAfferHighLight[arrObj[i].id];
+		}
+
+		for (var i = 0; i < arrParts.length; i++)
+		{
+
+		}
+
+		return strResult;
+	}
+
+	function HighLightSentence(strSentence, strKeyword)
+	{
+		var arrPart = [];
+		var uniqueKeyword = [];
+		var strKey;
+		var iIndex;
+		var strNewSentence;
+		var strLowerCaseSentence;
+		var arr = [];
+
+		strLowerCaseSentence = strSentence.toLowerCase();
+		//Xoa duplicate trong keyword
+		strKeyword = strKeyword.toLowerCase();
+		arrPart = strKeyword.split(" ");
+		for (var i = 0; i < arrPart.length; i++)
+		{
+			if(uniqueKeyword.indexOf(arrPart[i]) == -1)
+			{
+				uniqueKeyword.push(arrPart[i]);
+			}
+		}
+
+		strKey = uniqueKeyword[0];
+		strNewSentence = "";
+		var strTmp = strLowerCaseSentence;
+		for (var i = 0; i < arrPart.length; i++)
+		{
+			var regex = new RegExp(arrPart[i], "gi"), result, indices = [], origin = [];
+			while ( (result = regex.exec(strTmp)) ) {
+			    indices.push(result.index);
+			}
+
+			while ( (result = regex.exec(strLowerCaseSentence)) ) {
+			    origin.push(result.index);
+			}
+
+			iIndex = 0;
+			strNewSentence = "";
+			for (var j = 0; j < indices.length; j++)
+			{
+				strNewSentence += strTmp.substring(iIndex, indices[j]);
+				strNewSentence += "<b>";
+				strNewSentence += strSentence.substring(origin[j], origin[j] + arrPart[i].length);
+				strNewSentence += "</b>";
+				iIndex = indices[j] + arrPart[i].length;
+			}
+			strNewSentence += strTmp.substring(indices[indices.length - 1] + arrPart[i].length);
+			strTmp = strNewSentence;
+		}
+
+		return strTmp;
+	}
 
 	function ConvertQueryToJson(szQuery)
 	{
